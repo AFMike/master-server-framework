b0bbc3fa381fedf4c80318c5a308d019468480a2
Владимир <aevien@hotmail.com>
2020-05-11 12:58:06 +1000

    Update to 3.5.1

--- Assets/Barebones/Msf/Scripts/Msf/Msf.cs
+++ Assets/Barebones/Msf/Scripts/Msf/Msf.cs
@@ -12,7 +12,7 @@ namespace Barebones.MasterServer
         /// <summary>
         /// Version of the framework
         /// </summary>
-        public static string Version => "v3.5.0";
+        public static string Version => "v3.5.1";
 
         /// <summary>
         /// Just name of the framework
